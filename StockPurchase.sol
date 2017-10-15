3e9ea46886dee95d61c1268b34accb0378cfd815
George <georgek00012@gmail.com>
2017-10-15 14:52:56 -0700

    StockPurchase.sol

--- StockPurchase.sol
+++ StockPurchase.sol
@@ -1,19 +1,19 @@
-a solidity 0.4.9;
+pragma  solidity 0.4.9;
 
 contract StockPurchase {
 	    
-	    address public VNT;
-	        string public Agreement = "https://gateway.ipfs.io/ipfs/QmVp1R6yEJARm6mrGTzSpU8Lo4QGk4ZQbxWMRcniUQJS4z";
-		    string public auth = "When execute() is called the Vega Network is digitally signing the Ageement.";
+	address public VNT;
+
+	string public Agreement = "https://gateway.ipfs.io/ipfs/QmVp1R6yEJARm6mrGTzSpU8Lo4QGk4ZQbxWMRcniUQJS4z";
+
+	string public auth = "When execute() is called the Vega Network is digitally signing the Ageement.";
 		        
-		        
-		        function execute() public constant returns (string) {
-				        return auth;
-					    }
+	        
+	function execute() public constant returns (string) {
+		return auth;
+	}
 					        
-					        function() {
-							        if(msg.sender != VNT) throw;
-								        execute();
-									    }
-									        
+	function() {
+		if(msg.sender != VNT) throw;
+		execute();								}
 }
